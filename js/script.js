041e9689f84cf9cfc4cd98ac58688e0cd8422f56
WabsCod <waboyvan10@gmail.com>
2026-02-14 12:15:44 +0100

    initialisation du projet

---
+++ js/script.js
@@ -0,0 +1,61 @@
+/*================================toggle icon navbar================================ */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active'); 
+}
+
+
+/*================================scroll section active link================================ */
+let sectoins = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll =() => {
+    sectoins.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+/*================================remove toggle icon and navbar when click navbar link (scroll)================================ */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/*================================scroll reveal================================ */
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'} );
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'} );
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'} );
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'} );
+
+
+            /* type js mattboldt.github.io/typed.js/ */
+    let typed = new Typed('#type', {
+      strings: ['Developpeur Full Stack!', 'Designer UI/UX!', 'Trader!'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true
+    }); 
+
+
